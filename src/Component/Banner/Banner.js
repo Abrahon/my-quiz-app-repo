403f0e6f463aab0b9718fe60cd88670dca9a49df
Abrahon <90842196+Abrahon@users.noreply.github.com>
2022-10-12 23:30:50 +0600

    change the header name

--- src/Component/Banner/Banner.js
+++ src/Component/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
+// import Button from 'react-bootstrap/Button';
 import './Banner.css'
 
 const Banner = () => {
@@ -10,12 +10,8 @@ const Banner = () => {
                     <img src="https://img.freepik.com/free-vector/online-tutorials-concept_52683-37480.jpg?w=996&t=st=1665474726~exp=1665475326~hmac=b881a1835bb3baa651894fb93a5756bea0233600f94cb8774974a1cbb9d37c31" alt="" />
                 </div>
                 <div className="col-lg-6 bg-right ">
-                    <h1 className=''>
-                        For every student,
-                        every classroom.
-                        Real results.</h1>
+                    <h1 className=''>This is my first quiz App.This App very easy to use.</h1>
                     <p className='fs-5 my-4'>We’re a nonprofit with the mission to provide a free, world-class education for anyone, anywhere.</p>
-                    <Button className='px-5 py-2 fs-4 bg-color text-white' variant="">Learner Start Here</Button>{' '}
                 </div>
             </div>
         </div>
